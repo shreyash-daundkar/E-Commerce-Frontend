2a1bacd0dd4991f12078bac6bb8a925dca28b5f1
Shreyash Daundkar <productiveappa@gmail.com>
2024-07-22 11:25:19 +0530

    Added Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Cart from './components/Cart/Cart';
+import Header from './components/Header/Header';
+import ProductsList from './components/Product/ProductsList';
+import CartContextProvider from './context/CartContextProvider';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <CartContextProvider>
+    <Header />
+    <ProductsList />
+    <Cart />
+   </CartContextProvider>
+   </>
   );
 }
 
